df20941e44a9d6852ce1e87ca19eab6377fae214
tom-huntington <55266932+tom-huntington@users.noreply.github.com>
2024-02-03 10:51:03 +1300

    trying to deploy

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,19 @@
 /// <reference types="astro/client" />
 /// <reference types="simple-stack-form/types" />
+
+
+type ENV = {
+  SERVER_URL: string;
+  D1: import('@cloudflare/workers-types/experimental').D1Database;
+};
+
+type Runtime = import('@astrojs/cloudflare').AdvancedRuntime<ENV>;
+
+declare namespace App {
+  interface Locals extends Runtime {
+    user: {
+      name: string;
+      surname: string;
+    };
+  }
+}
